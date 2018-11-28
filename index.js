18b4584cf1915825ed477fc1db62cbeea6a6d29f
Sean O'Hollaren <Sean.OHollaren2@nike.com>
2018-11-28 09:09:38 -0800

    There is just one thing I need

---
+++ index.js
@@ -0,0 +1,27 @@
+const request = require('request-promise');
+const Promise = require('bluebird');
+
+const requestUrl = 'https://docs.google.com/forms/d/e/1FAIpQLSc46bByCUYFvKtOiKfdOr83jnSNwVhx2I6hOa-SoG9JEZAAhg/formResponse';
+const numRequests = 1000;
+let counter = 0;
+
+Promise.map(new Array(numRequests), () => allIWant(), {
+  concurrency: 15
+} );
+
+function allIWant() {
+  console.log(`Requesting All I Want for Christmas is You... #${counter++}`);
+  return request
+  .post(requestUrl, {
+    form: {
+      'entry.1299052505':'All I Want for Christmas is You', 
+      'entry.1639397315': 'Mariah Carey'
+    }
+  })
+  .then(() => {
+    console.log('    Success!');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+}
